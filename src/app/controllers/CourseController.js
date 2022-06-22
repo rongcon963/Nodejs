ff05ec4499d89eb8d3ed1dc0201d99543a8c40de
Tuan pham <tuanpham@chidoanh.com>
2022-06-22 12:59:22 +0700

    add soft delete, restore trash

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -25,7 +25,7 @@ class CourseController {
         // res.json(req.body);
         const course = new Course(formData);
         course.save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
         //res.send('Course Save');
     }
@@ -49,7 +49,21 @@ class CourseController {
 
     // [DELETE] /courses/:id
     destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+    
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next);
     }
--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,8 +4,17 @@ const {multipleMongooseToObject} = require('../../util/mongoose')
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
+        Course.find({  })
             .then(courses => res.render('me/stored-courses', {
+                courses: multipleMongooseToObject(courses)
+            }))
+            .catch(next);
+    }
+
+    // [GET] /me/trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({  })
+            .then(courses => res.render('me/trash-courses', {
                 courses: multipleMongooseToObject(courses)
             }))
             .catch(next);
