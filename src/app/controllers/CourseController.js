219ce2efdc2481a8f6c00698662d76dbf8b00ed0
Tuan pham <tuanpham@chidoanh.com>
2022-06-23 14:13:05 +0700

    Add select all box

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -67,6 +67,36 @@ class CourseController {
             .then(() => res.redirect('back'))
             .catch(next);
     }
+
+    // [POST] /courses/handle-form-actions
+    handleFormActions(req, res, next) {
+        switch(req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json( { message: 'Action is invalid'} );
+        }
+    }
+
+    handleFormTrash(req, res, next) {
+        switch(req.body.action) {
+            case 'restore':
+                Course.restore({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            case 'delete':
+                Course.deleteOne({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json( { message: 'Action is invalid'} );
+        }
+    }
 }
 
 module.exports = new CourseController();
