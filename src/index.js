96ee636a1987feb61c5db6177c783d0668911248
Tuan pham <tuanpham@chidoanh.com>
2022-07-08 13:51:51 +0700

    Add sorting field course

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const handlebars  = require('express-handlebars');
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -21,6 +22,9 @@ app.use(express.json())
 
 app.use(methodOverride('_method'))
 
+// Custom middlewares
+app.use(SortMiddleware)
+
 // HTTP logger
 //app.use(morgan('combined'))
 
@@ -29,6 +33,24 @@ app.engine('hbs', handlebars.engine({
   extname: '.hbs',
   helpers: {
     sum: (a, b) => a + b,
+    sortable: (field, sort) => {
+      const sortType = field === sort.column ? sort.type : 'default';
+      
+      const icons = {
+        default: 'oi oi-elevator',
+        asc: 'oi oi-sort-ascending',
+        desc: 'oi oi-sort-descending',
+      }
+      const types = {
+        default: 'desc',
+        asc: 'desc',
+        desc: 'asc',
+      }
+
+      const icon = icons[sortType];
+      const type = types[sortType];
+      return `<a href="?_sort&column=${field}&type=${type}"><span class="${icon}"></span></a>`;
+    }
   }
 }));
 app.set('view engine', 'hbs');
